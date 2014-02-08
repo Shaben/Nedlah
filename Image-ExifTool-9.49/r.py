531526f2d76588dc0a09581dbaf12a53a52dae45
Gabe <gabe1118@yahoo.com>
2014-02-08 16:39:10 -0500

    Removed all files

--- Image-ExifTool-9.49/r.py
+++
@@ -1,9 +0,0 @@
-import subprocess;
-var = "ExifTool.jpg"
-pipe = subprocess.Popen(["./exiftool", var], stdin=subprocess.PIPE)
-result = pipe.stdout.read();
-print result
-fileobj = open(var+".txt", "r+");
-fileobj.write(result);
-fileobj.close();
-pipe.stdin.close()
